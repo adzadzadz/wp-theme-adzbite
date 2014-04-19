87917d265636486a514ff68663b67eb253471fa0
Adz <storytellercontact2@gmail.com>
2014-04-19 18:11:34 +0800

    Added FB Like Page js functionality.

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,17 @@ $(document).ready(function() {
   }); 
    
 });
+
+
+
+/*===================================================================================
+  ===============================FACEBOOK LIKE PLUGIN================================
+  =================================================================================== */
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=272663612872931";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
